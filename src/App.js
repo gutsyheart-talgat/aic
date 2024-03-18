b51b87573479eb7731705b42b1d49ccb355599e9
Talgat <dzhailikeevtalgat@gmail.com>
2024-03-18 11:30:35 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Route,Router,Routes } from 'react-router-dom';
+import About from './components/About/about'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='' element={<About/>}/>
+        <Route path='Kyrgyzstan'/>
+        <Route path='Contacts'/>
+        <Route path='Services'/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/About/about.js
@@ -0,0 +1,8 @@
+import React from "react";
+import style from './about.module.css'
+function About(){
+    return(
+        <div className={style.back}></div>
+    )
+}
+export default About
